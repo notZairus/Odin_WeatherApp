df0b51b79056b1737fe27a21b6fa38a014c9dfcc
notZairus <notzairus@gmail.com>
2024-07-25 21:57:54 +0700

    started

--- src/script.js
+++ src/script.js
@@ -1,1 +1,32 @@
-import '../css/style.css';
+import './style.css';
+
+
+async function fetchWeatherData (location) {
+  try {
+    
+    let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}}?unitGroup=us&key=2YUX44XCGJ9DV4KRCSQH7SPUR&contentType=json`);
+    
+    if (! response.ok) {
+      throw Error('Location not found.');
+    }
+
+    let data = await response.json();
+    console.log(data);
+
+  } 
+  catch(error) {
+    console.log(error);
+  }
+}
+
+fetchWeatherData('Bulacan');
+
+
+
+//WEATHER API
+//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bulacan?unitGroup=us&key=YOUR_API_KEY&contentType=json
+
+//API KEY
+//2YUX44XCGJ9DV4KRCSQH7SPUR
+
+
