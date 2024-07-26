f1ed28432e8e79b4580e0ee687c064d5d15e8b47
notZairus <notzairus@gmail.com>
2024-07-26 21:56:37 +0700

    ewan

---
+++ src/js (import)/icons.js
@@ -0,0 +1,39 @@
+
+
+//WEATHER CONDITION ICONS
+import clearDay from '../icons/clear-day.svg';
+import clearNight from '../icons/clear-night.svg';
+import cloudy from '../icons/cloudy.svg';
+import fog from '../icons/fog.svg';
+import hail from '../icons/hail.svg';
+import partlyCloudyDay from '../icons/partly-cloudy-day.svg';
+import partlyCloudyNight from '../icons/partly-cloudy-night.svg';
+import rainSnowShowersDay from '../icons/rain-snow-showers-day.svg';
+import rainSnowShowersNight from '../icons/rain-snow-showers-night.svg';
+import rainSnow from '../icons/rain-snow.svg';
+import rain from '../icons/rain.svg';
+import showersDay from '../icons/showers-day.svg';
+import showersNight from '../icons/showers-night.svg';
+import snowShowersDay from '../icons/snow-showers-day.svg';
+import snowShowersNight from '../icons/snow-showers-night.svg';
+import snow from '../icons/snow.svg';
+
+
+export const ICONS = {
+  clearDay,
+  clearNight,
+  cloudy,
+  fog,
+  hail,
+  partlyCloudyDay,
+  partlyCloudyNight,
+  rainSnowShowersDay,
+  rainSnowShowersNight,
+  rainSnow,
+  rain,
+  showersDay,
+  showersNight,
+  snowShowersDay,
+  snowShowersNight,
+  snow
+};
--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
 import './style.css';
+import { format } from 'date-fns';
+import { ICONS } from './js (import)/icons';
+
 
 
 async function fetchWeatherData (location) {
   try {
-    
     let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}}?unitGroup=us&key=2YUX44XCGJ9DV4KRCSQH7SPUR&contentType=json`);
     
     if (! response.ok) {
@@ -11,22 +13,38 @@ async function fetchWeatherData (location) {
     }
 
     let data = await response.json();
-    console.log(data);
-
+    return data;
   } 
   catch(error) {
     console.log(error);
   }
 }
 
-fetchWeatherData('Bulacan');
+function displayData (data) {
+  let { resolvedAddress: location } = data;
+  let locationParts = location.split(", ");
+  document.getElementById('location').textContent = locationParts[0] + ", " + locationParts[2];
+
+  let dateTime = new Date();
+  document.getElementById('datetime').textContent = format(dateTime, 'EEE') + " | " + format(dateTime, "PP") + " | " + format(dateTime, "h b");
+
+  let currentCondition = data.currentConditions.icon;
+}
+
+function changeBgColor () {
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.background = randomColor;
+}
 
 
+setInterval(changeBgColor, 2000);
 
-//WEATHER API
-//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bulacan?unitGroup=us&key=YOUR_API_KEY&contentType=json
+document.getElementById('enterBtn').addEventListener('click', async () => {
+  let input = document.getElementById('locationInput');
 
-//API KEY
-//2YUX44XCGJ9DV4KRCSQH7SPUR
+  let data = await fetchWeatherData(input.value);
+  displayData(data);
 
+  input.value = "";
+})
 
